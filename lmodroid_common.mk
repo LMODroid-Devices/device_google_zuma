392f502d67355af2008197b52dd1134674d20ba5
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:30:28 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_common.mk
+++ lmodroid_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-lmodroid
 
 # AiAi Config
 PRODUCT_COPY_FILES += \
